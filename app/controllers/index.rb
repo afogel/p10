aef5f6c889169a26ab6879416e5e10150a3a2a14
Ariel Fogel <fogeltine@gmail.com>
2014-07-31 16:03:44 -0700

    add raises to test POST '/texts' route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,10 +13,14 @@ post '/' do
 end
 
 post '/texts' do
+  raise "ERROR MOTHERFUCKER"
   @user = User.find(params[:user_id])
   user_and_message = [{ :message => params[:message],
                        :phone_number => @user.phone_number}]
+  raise "ERROR MOTHERFUCKER"
   test_message = TwilioWrapper.new(user_and_message)
+  raise "ERROR MOTHERFUCKER"
   test_message.send!
+  raise "ERROR MOTHERFUCKER"
   redirect "/users/#{@user.id}"
 end
