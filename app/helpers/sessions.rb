d8d2df9acc0d3d39e104957f4ae01026a354dcd3
Ariel Fogel <fogeltine@gmail.com>
2014-07-31 16:21:39 -0700

    derrr  more debugging

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,11 +1,9 @@
 def current_user
   p "I'm a current_user"
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  p "I'm out of the current_user business"
 end
 
 def logged_in?
   p "I'm logged in"
   current_user
-  p "I was truthy"
 end
