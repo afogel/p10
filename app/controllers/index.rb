9c321df3b72974f6a880d9de9b159e843ce81920
Ariel Fogel <fogeltine@gmail.com>
2014-07-31 16:46:42 -0700

    Remove all the stupid putses from my routes. FUCK HEROKU AGAIN. MANY MANY TIMES HARD YES.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,12 +14,9 @@ end
 
 post '/texts' do
   @user = User.find(params[:user_id])
-  p "I passed the AR lookup"
   user_and_message = [{ :message => params[:message],
                        :phone_number => @user.phone_number}]
   test_message = TwilioWrapper.new(user_and_message)
-  p "I created a new TwilioWrapper"
   test_message.send!
-  p "I sent a twiliowrapper"
   redirect "/users/#{@user.id}"
 end
--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,9 +1,7 @@
 def current_user
-  p "I'm a current_user"
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
 
 def logged_in?
-  p "I'm logged in"
   current_user
 end
