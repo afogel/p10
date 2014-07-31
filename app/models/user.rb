5078a104f218791d96b23b5ea1afde157d50ec13
Ariel Fogel <fogeltine@gmail.com>
2014-07-30 22:57:48 -0700

    Added relation between users and contacts to user model. A- doiyyyyy.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :contacts
   # Remember to create a migration!
   # scope :needs_messages_sent, -> {|user| }
-  def needs_reminder_sent
+  def needs_reminder_sent?
 
   end
 end
